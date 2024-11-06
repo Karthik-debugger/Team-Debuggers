a6aca68d6ddaaeea69ee11e00d50b2e7cf827edb
Haritha-debugger <hariharithareddy9@gmail.com>
2024-11-05 19:46:28 -0600

    Charan

---
+++ Sign/main.py
@@ -0,0 +1,13 @@
+from capture_video import capture_video
+from hand_detection import hand_detection
+from gesture_recognition import recognize_gesture
+
+if __name__ == "__main__":
+    print("Running Task 1: Capture Video")
+    capture_video()
+    
+    print("Running Task 2: Hand Detection")
+    hand_detection()
+
+    print("Running Task 3: Gesture Recognition")
+    recognize_gesture()
